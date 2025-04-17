9da3c0886c9fd77e61066610c49fc7f54547be29
Hester <lhester7@una.edu>
2025-04-17 16:39:55 -0500

    bmj

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,8 +1,24 @@
 // A $( document ).ready() block.
 $( document ).ready(function() {
-    console.log( "ready!" );
-  
-  //more code here...
+  console.log( "ready!" );
+
+//more code here...
 });
 
-console.log("This is Garrett Bouldin\'s Web Profile");
+console.log("This is the official SOBIE website!");
+
+const updateData = () => {
+return {
+  question: {
+    email: document.getElementById("inputEmail").value || "N/A",
+    questionAsked: document.getElementById("question").value || "N/A", 
+  },    
+};
+};
+
+const submitQuestionBtn = document.getElementById("submitQuestionBtn");
+
+submitQuestionBtn.addEventListener("click", () => {
+const updatedData = updateData();
+console.log(JSON.stringify(updatedData, null, 2));
+});
