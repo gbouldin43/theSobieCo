391256c0f19bf791dfd4f1a77b7efb1983264514
Garrett Bouldin <gbouldin@una.edu>
2025-04-22 13:54:33 -0500

    Delete public directory

--- public/scripts/script.js
+++
@@ -1,8 +0,0 @@
-// A $( document ).ready() block.
-$( document ).ready(function() {
-    console.log( "ready!" );
-  
-  //more code here...
-});
-
-console.log("This is the official SOBIE website!");
